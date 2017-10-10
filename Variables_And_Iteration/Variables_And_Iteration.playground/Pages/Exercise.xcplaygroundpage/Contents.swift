a34ddaef22875faf94a0ad21162203359d764860
Julia Ruiz Rovelo <jruizrovelo@lcs-mba3257.lcs.on.ca>
2017-10-10 11:22:35 -0400

    progress

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -17,11 +17,19 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 //: ## Add your code below
-
-
-
+// dots
+for x in stride(from: 0, through: 300, by: 25) {
+    for y in stride(from: 0, to: 300, by: 25) {
+        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
+}
+}
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 30, alpha: 100)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 700, height: 400)
+canvas.fillColor = Color.init(hue: <#T##Int#>, saturation: <#T##Int#>, brightness: <#T##Int#>, alpha: <#T##Int#>)
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 700, height: 175)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
 PlaygroundPage.current.liveView = canvas.imageView
+
